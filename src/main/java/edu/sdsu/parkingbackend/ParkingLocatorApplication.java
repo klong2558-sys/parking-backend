289a4fc477a42bc8f43dea3d344d2039c8ee58ef
klong2558-sys <klong2558@sdsu.edu>
2025-10-20 00:12:53 -0700

    10/20 12:15 AM

--- src/main/java/edu/sdsu/parkingbackend/ParkingLocatorApplication.java
+++ src/main/java/edu/sdsu/parkingbackend/ParkingLocatorApplication.java
@@ -2,13 +2,18 @@ package edu.sdsu.parkingbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class ParkingLocatorApplication {
+    private static final Logger log = LoggerFactory.getLogger(ParkingLocatorApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(ParkingLocatorApplication.class, args);
+        log.info("Parking Backend started successfully.");
     }
+
 
 
 }
